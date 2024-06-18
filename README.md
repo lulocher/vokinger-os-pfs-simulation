71557e171894d298ea67a71687b8af13c88bc1c3
Luca Locher <luclocher@ethz.ch>
2024-06-18 15:18:05 +0200

    Updated description and README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Simulation of clinical trials in oncology
 
-This repository contains all code related to the paper "Why effect sizes are systematically larger for PFS than for OS in clinical trials for cancer drugs" by Locher, Serra-Burriel, Nussli & Vokinger (unpublished manuscript). For more detailed information, please refer to the [paper](add link).
+This repository contains all code related to the paper "Why effect sizes are systematically larger for PFS than for OS in clinical trials for cancer drugs" by L Locher, M Serra-Burriel, E Nussli & KN Vokinger (unpublished manuscript). For more detailed information, please refer to the paper.
 
-The subfolder '/application' contains the code for the published [shiny application](add link). The subfolder './examples' contains the code that produced the examples in the paper. Please note that the code underlying the simulation is duplicated in the two folders due to issues with the deployment of the shiny application.
+The subfolder '/application' contains the code for the published [shiny application](https://lulocher.shinyapps.io/oncology-trial-simulator/). The subfolder './examples' contains the code that produced the examples in the paper. Please note that the code underlying the simulation is duplicated in the two folders due to issues with the deployment of the shiny application.
--- application/app.py
+++ application/app.py
@@ -242,11 +242,9 @@ with ui.accordion(id="acc", open=["Introduction", "Parametrization", "Results"])
         with ui.div(class_="py-4 mx-auto text-left"):
             ui.markdown(
                 f"""
-                This application simulates the outcome of a clinical trial in oncology using the framework presented in the paper "Why effect sizes are systematically larger for PFS than for OS in clinical trials for cancer drugs" (Locher, Serra-Burriel, Nussli & Vokinger, unpublished manuscript). 
-                Parameters for the simulation can be set below. When a parameter is changed, a new trial is automatically simulated. 
-                Note that the variance in the results is due to the stochastic nature of the simulation and decreases with larger numbers of participants and longer duration. The outcomes for progression-free survival (PFS) and overall survival (OS) are displayed in the Results tab. 
-                The purpose of this application is to allow users to interact with the simulation and observe the impact of different parameters on the outcome of the trial.
-                More detailed information can be found in the paper. The underlying code is open-source and available on [GitHub]({link_repo}).
+                This application simulates the outcome of a clinical trial for cancer drugs using the framework presented in the paper "Why effect sizes are systematically larger for PFS than for OS in clinical trials for cancer drugs" (L Locher, M Serra-Burriel, E Nussli & KN Vokinger, unpublished manuscript).
+                Parameters for the simulation can be set below. When a parameter is changed, a new trial is automatically simulated. Note that the variance in the results is because of the stochastic nature of the simulation and decreases with larger numbers of participants and longer duration. The outcomes for progression-free survival (PFS) and overall survival (OS) are displayed in the Results tab. 
+                The objective of this application is to enable users to interact with the simulation and observe the impact of different parameters on the outcome of the trial. More detailed information can be found in the paper. The underlying code is open-source and available on [GitHub]({link_repo}).
                 """
             )
 
